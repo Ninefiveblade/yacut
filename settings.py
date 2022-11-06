6d827b0dda90c3f9c42d2f7f083c2593eb82efc6
Ninefiveblade <cherep1.92@yandex.ru>
2022-11-06 16:05:25 +0300

    Первое ревью

--- settings.py
+++ settings.py
@@ -1,9 +1,12 @@
 """ Flask settings module """
 
 import os
+import uuid
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    """ Define configs for flask app """
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") or 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY") or uuid.uuid4().hex
